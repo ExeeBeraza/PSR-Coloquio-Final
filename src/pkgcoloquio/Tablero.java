aa4073bc64a042d741f8d66206807bbb058bf3b2
nacho-meli <ignacio.martinezberaza@mercadolibre.com>
2022-11-23 02:47:43 -0300

    Finalizacion primera version concurrente del juego de la vida.

--- src/pkgcoloquio/Tablero.java
+++ src/pkgcoloquio/Tablero.java
@@ -97,6 +97,8 @@ public class Tablero extends JPanel implements ComponentListener, MouseListener,
 
     @Override /*-->*/
     public void run() {
+        long tiempoInicio = System.currentTimeMillis();
+
         // Defino el tablero con valores boleanos --> ¿Evoluciono o no?
         boolean[][] gameBoard = new boolean[d_gameBoardSize.width + 2][d_gameBoardSize.height + 2];
 
@@ -123,18 +125,23 @@ public class Tablero extends JPanel implements ComponentListener, MouseListener,
 
         /* AGREGO EL NUEVO ARRAY AL PRINCIPAL */
         point.addAll(celulasobrevivientes);
-        System.out.println("Las celulas sobrevivientes despues del thread...");
-        System.out.println(celulasobrevivientes.size());
 
         repaint();
+
+        /* TODO: Agregar aca una marca de tiempo para medir el tiempo total de ejecucion */
+        long tiempoFinal = System.currentTimeMillis() - tiempoInicio;
 
         try {
             /* Duermo el hilo 1000 / en el contenido de movimiento por segundo --->3 */
             Thread.sleep(1000 / i_movesPerSecond);
-            /* Dejo correr */
-            run();
         } catch (InterruptedException ex) {
             System.out.println(ex.toString());
+            /* TODO: En caso de que no se pare el hilo. Como lo puedo parar a la fuerza? */
+        } finally {
+            System.out.printf("El tiempo final para el ciclo parcial es de: %f milisegundos\n",(float) tiempoFinal);
+
+            /* Dejo correr */
+            run();
         }
     }
 
